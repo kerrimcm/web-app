bb9dda0d60f4a1485bb25b1e968ccafe372e0c81
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-12 18:03:39 +0100

    Sinatra working and added reloader

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+# http://localhost:9494/
+
+set :port, 9494
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'Welcome to our website'
+end 
