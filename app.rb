330aa699a614cbb544df3d587f871c1f87aa152b
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-13 14:40:38 +0100

    Added named-cat path with params

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/secret' do
   'Welcome to our secret website'
 end 
 
-get '/cat' do
-  @name_randomizer = ["Amigo", "Mittens", "Almond"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Mittens", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
